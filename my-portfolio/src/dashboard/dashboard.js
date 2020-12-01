556993730729d89dca249d7f0d977955ce711355
shareed <shanreed6@gmail.com>
2020-12-01 13:45:32 -0800

    styling dashboard menu, icon and links done

--- my-portfolio/src/dashboard/dashboard.js
+++ my-portfolio/src/dashboard/dashboard.js
@@ -1,5 +1,8 @@
 import { Route } from 'react-router';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faHome, faUser, faProjectDiagram, faTasks, faFile } from '@fortawesome/free-solid-svg-icons'
 import me from '../assets/me.jpg';
 
 import styled from "styled-components";
@@ -24,7 +27,8 @@ const Dashdiv = styled.div`
       height: 100vh;
       width: 35vw;
       background-color: #120078;
-      padding: 1rem;
+    
+      
       /* text-align: center; */
       /* opacity: 40%; */
       /* font-size: 3.2rem; */
@@ -40,10 +44,13 @@ const Dashdiv = styled.div`
 `
 
 const Header = styled.header` 
+   margin-bottom: 2rem;
+   padding: 1rem;
    display: flex;
    justify-content: center;
    align-items: center;
    color: #FECD1A;
+   border-bottom: .3rem outset #FECD1A;
    `
 
 
@@ -62,7 +69,7 @@ const IntroDiv = styled.div`
    `
 
 const MyNameIntro= styled.h2`
-   border-bottom: .2rem solid #FECD1A;
+   border-bottom: .2rem dotted #FECD1A;
    font-size: 2rem;
    text-align: center;
    padding-bottom: .5rem;
@@ -73,9 +80,36 @@ const MyIntro= styled.h2`
    text-align: center;
    padding-top: .5rem;
    `
+
+const Icons = styled.div`
+   font-size: 2rem;
+   margin-top: .7rem; 
+   display:flex;
+   justify-content: space-between;
+   `
+
+const LinkContainer = styled.div` 
+   display: flex;
+   /* justify-content: space-between; */
+   `
+
+
 const MenuLinks = styled.div` 
+border: 2px solid red;
+   height: 30vh;
          display: flex;
          flex-direction: column;
+         justify-content: space-between;
+         a {
+            padding: 1rem;
+            font-size: 2rem;
+            :hover{
+               background: red;
+            }
+         }
+         .faHome {
+               margin-right: 2rem;
+            }
 `
 
 
@@ -85,18 +119,51 @@ function Dashboard() {
            <HamburgerMenu />
            <Dashdiv >
               <Header>
+                 <div>
                <MyImage src = { me }/>
+               <Icons>
+               <FontAwesomeIcon icon={faTwitter} />
+               <FontAwesomeIcon icon={faLinkedin} />
+               <FontAwesomeIcon icon={faGithub} />
+               </Icons>
+               </div>
                <IntroDiv>
                <MyNameIntro>Shannon Reed</MyNameIntro>
                <MyIntro>Full Stack Web Developer</MyIntro>
                </IntroDiv>
              </Header>
                   <MenuLinks >
-                     <Link>Home</Link>
-                     <Link>Menu</Link>
-                     <Link>Projects</Link>
-                     <Link>Skills</Link>
-                     <Link>About</Link>
+                     <Link>
+                        <LinkContainer>
+                        <FontAwesomeIcon icon={ faHome } />
+                        Home
+                        </LinkContainer>
+                     </Link>
+                     {/* <Link>Menu</Link> */}
+                     <Link>
+                        <LinkContainer>
+                        <FontAwesomeIcon icon={faProjectDiagram} />
+                        Projects
+                        </LinkContainer>
+                     </Link>
+                     <Link>
+                        <LinkContainer>
+                        <FontAwesomeIcon icon={faTasks} />
+                        Skills
+                        </LinkContainer>
+                     </Link>
+                     <Link>
+                        <LinkContainer>
+                        <FontAwesomeIcon icon={faUser}/>
+                        About
+                        </LinkContainer>
+                     </Link>
+                     <a href = 'https://drive.google.com/file/d/15I7pu_6iLF3HoYZej2xUHpPHhAGLcssU/view' >
+                           <LinkContainer>
+                        <FontAwesomeIcon icon={faFile} />
+                        Resume
+                        </LinkContainer>
+                     </a>
                   </MenuLinks>
            </Dashdiv>
           
