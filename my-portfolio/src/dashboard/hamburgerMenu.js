02ad9a9c18e64a38573e28366e81a46ad8bedc99
shareed <shanreed6@gmail.com>
2020-11-30 22:52:14 -0800

    adding hamburger menu logic

--- my-portfolio/src/dashboard/hamburgerMenu.js
+++ my-portfolio/src/dashboard/hamburgerMenu.js
@@ -6,8 +6,52 @@ import styled from "styled-components";
 
 
 import './hamburgerMenu.css';
+const Container = styled.div`
+    position: relative;
+    @media(min-width: 800px) {
+        display: none;
+    }
+    `
+const HamburgerButton = styled.div` 
+    position: relative;
+    z-index: 99;
+    `
 
+const CloseButton = styled.div` 
+    margin: 0 0 40px 0;
+    display: flex;
+    justify-content: flex-start;
+    `
 
+const HamburgerMenuNav = styled.nav` 
+    .show{
+        left: 0;
+    }
+    `
+
+const MenuList = styled.ul` 
+    background: #221F3B;
+    padding: 0;
+    margin-top: 1.8rem;
+    font-size: 2.5rem;
+    display: flex;
+    flex-direction: column;
+    margin-left: -10px;
+    `
+
+const MenuListItem = styled.li` 
+    list-style: none;
+    margin: 0 auto;
+    width: 99%;
+    border-bottom: 1px solid #6f4a8e;
+    `
+
+const MenuListItemLink = styled.a` 
+    text-decoration: none;
+    color: #FFF;
+    display: flex;
+    padding: 10px 16px;
+    `
 
 class HamburgerMenu extends Component {
     constructor(props) {
@@ -27,31 +71,31 @@ class HamburgerMenu extends Component {
             console.log("state:", this.state);
             const { isMenuOpen } = this.state;// same as --> const  isMenuOpen = this.state.isMenuOpen;
       return (
-               <div className = 'hamburger-menu-container'>
-                  <div className = 'hamburger-menu-button' onClick = { this.handleClick }>
+              <Container>
+                  <HamburgerButton onClick = { this.handleClick }>
                   <FontAwesomeIcon icon={faBars} />
-                  </div>
+                  </HamburgerButton>
                            {/* //If there are items inside and */}
                            { menuItems.length && (
                               // gives a className of nav and if menu is open add class show otherwise add class hide
-                                 <nav className = {`hamburger-menu-nav ${isMenuOpen ? 'show': ''}`}>
-                                       <div className = 'hamburger-menu-close-button' onClick = { this.handleClick }>
+                                 <HamburgerMenuNav className = {`hamburger-menu-nav ${isMenuOpen ? 'show': ''}`}>
+                                       <CloseButton onClick = { this.handleClick }>
                                           <FontAwesomeIcon icon={faTimes} />
-                                      </div>
-                                    <ul className = 'hamburger-menu-items'>
+                                      </CloseButton>
+                                    <MenuList>
                                           {menuItems.map(item => (
-                                             <li className = 'hamburger-menu-list-item' key = {item.label}>
-                                             <a href = {item.url} className = 'hamburger-menu-link' >
+                                             <MenuListItem key = {item.label}>
+                                             <MenuListItemLink href = {item.url}>
                                                    {item.label}
-                                                </a>
-                                             </li>
+                                                </MenuListItemLink>
+                                             </MenuListItem>
                                              ))}
-                                    </ul>
-                                    </nav>
+                                    </MenuList>
+                                    </HamburgerMenuNav>
 
                               
                            ) }
-                  </div>
+                  </Container>
          )
       }
 }
